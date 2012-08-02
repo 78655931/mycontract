ec6730a9ad25acc7baa44a4ddb2bc0a81344c6f9
liufei <78655931@qq.com>
2012-08-02 13:14:13 +0800

    忽略不需要提交的件

--- Admin.bak/Lib/Action/TestAction.class.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class TestAction extends CommonAction {
-		public function index(){
-			$this->display();
-		}
-}
--- Admin.bak/Lib/Model/ReservationModel.class.php
+++
@@ -1,5 +0,0 @@
-<?php
-// 用户模型
-class ReservationModel extends AdvModel {
-	}
-?>
